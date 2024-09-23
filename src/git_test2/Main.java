76f27b792d8f3b8ecd3875907da519a43fa2389e
Park, Seohyun <tjgus6279@naver.com>
2024-09-24 00:20:19 +0900

    Hello 클래스 수정

--- src/git_test2/Main.java
+++ src/git_test2/Main.java
@@ -1,7 +1,10 @@
-package git_test2; 
+package git_test2;
+ 
 public class Main { 
-public static void main(String[] args) { 
-Hello h = new Hello(); 
-h.say(); 
+ 
+    public static void main(String[] args) { 
+        Hello h = new Hello("hello world"); 
+        h.say(); 
+    } 
+ 
 } 
-}
